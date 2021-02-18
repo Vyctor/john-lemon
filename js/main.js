ee90eff363ca19b54475e05ac4881b37b3f3a051
Vyctor <dev.vyctor@gmail.com>
2021-02-18 14:05:20 -0300

    correcao preloader

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 const btnContact = document.querySelector(".jl-btn-contact");
 
+window.addEventListener("load", function () {
+  const pagePreloader = document.querySelector(".jl-preloader");
+  pagePreloader.classList.add("jl-fade-out");
+
+  setTimeout(() => {
+    pagePreloader.style.display = "none";
+  }, 2000);
+});
+
 btnContact.addEventListener("click", function () {
   const boxContact = document.querySelector(".jl-contact-info");
   boxContact.classList.toggle("jl-is-open");
