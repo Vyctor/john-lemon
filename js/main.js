a06ed6df3919ede3c7d9b29f2ed19c3335bffeb7
Vyctor <dev.vyctor@gmail.com>
2021-02-18 13:04:46 -0300

    contato - fazendo toggle click no botao de contato

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const btnContact = document.querySelector(".jl-btn-contact");
+
+btnContact.addEventListener("click", function () {
+  const boxContact = document.querySelector(".jl-contact-info");
+  boxContact.classList.toggle("jl-is-open");
+  this.classList.toggle("jl-change-icon");
+});
