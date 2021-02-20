87cab3b7cfe393b4104ddfb4bdb3e9c5e0ae2116
Vyctor <dev.vyctor@gmail.com>
2021-02-20 18:26:43 -0300

    modal - fazendo o javascript

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const btnContact = document.querySelector(".jl-btn-contact");
+const toggleModal = document.querySelectorAll(".jl-toggle-modal");
 
 window.addEventListener("load", function () {
   var pagePreloader = document.querySelector(".jl-preloader");
@@ -9,8 +10,21 @@ window.addEventListener("load", function () {
   }, 2000);
 });
 
+// Abrindo e fechando informacoes de contato
 btnContact.addEventListener("click", function () {
   const boxContact = document.querySelector(".jl-contact-info");
   boxContact.classList.toggle("jl-is-open");
   this.classList.toggle("jl-change-icon");
 });
+
+// Abrindo e fechando modal de orcamento
+for (let i = 0; i < toggleModal.length; i++) {
+  toggleModal[i].addEventListener("click", function () {
+    const overlay = document.querySelector(".jl-overlay");
+    const modalOrcamento = document.querySelector("#jl-modal-orcamento");
+
+    overlay.classList.toggle("jl-is-open");
+    modalOrcamento.classList.toggle("jl-is-open");
+    modalOrcamento.classList.toggle("jl-slideTopIn");
+  });
+}
