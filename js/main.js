5eb5aabba6b29f3219625ffc309fb84b96e5590d
Vyctor <dev.vyctor@gmail.com>
2021-02-20 18:53:56 -0300

    waypoints - animando elementos on scroll

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const btnContact = document.querySelector(".jl-btn-contact");
 const toggleModal = document.querySelectorAll(".jl-toggle-modal");
+const myScrollDown = document.querySelector(".jl-scroll-down");
 
 window.addEventListener("load", function () {
   var pagePreloader = document.querySelector(".jl-preloader");
@@ -28,3 +29,12 @@ for (let i = 0; i < toggleModal.length; i++) {
     modalOrcamento.classList.toggle("jl-slideTopIn");
   });
 }
+
+// Animando Elementos on Scrool com Waypoints
+const waypoint = new Waypoint({
+  element: myScrollDown,
+  handler: function () {
+    myScrollDown.classList.toggle("jl-fade-out");
+  },
+  offset: "80%",
+});
